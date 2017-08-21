fae846782557b5dc7f37b31e484017db74f0d880
Administrator <Administrator@A-201704201602>
2017-08-21 19:45:38 +0800

    合并

--- 20170821_web3/src/main/java/com/qianfeng/test/Test.java
+++ 20170821_web3/src/main/java/com/qianfeng/test/Test.java
@@ -1,14 +1,22 @@
 package com.qianfeng.test;
 
 public class Test {
-	int x = 9;
+	// int x = 9;
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		System.out.println();
+	}
 
 	public int sum(int x, int y) {
 		return x + y;
 	}
 
-	public static void main(String[] args) {
-		Test test = new Test();
-		System.out.println();
+	public int sub(int x, int y) {
+		return x - y;
+	}
+
+	public int div(int x, int y) {
+		return x / y;
 	}
 }
